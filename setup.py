0952ade27b3d9cfa021cc3e461535e4ce0196a2f
thomasdonlon <thomasdonlonii@gmail.com>
2024-05-17 14:01:44 -0500

    v1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="peebee",
-    version="0.0.8",
+    version="1.0.0",
     author="Tom Donlon",
     author_email="thomas.donlon@uah.edu",
     description="A python package for the intersection of pulsar accelerations and Galactic structure",
