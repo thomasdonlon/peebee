0efd562fa361cd51cd647396ee5b05922210e889
thomasdonlon <thomasdonlonii@gmail.com>
2026-03-03 14:13:14 -0600

    testing build config hangup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,6 +78,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'piccolo_theme'
+#html_theme = 'piccolo_theme'
 html_static_path = ['_static']
 
